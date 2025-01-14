e4f8756baffc2fffdfd36903a4f4d2eca1c8b3b3
hammadsadi <hammad.sadi@yahoo.com>
2025-01-14 12:33:15 +0600

    Token Set In header and Semester Get Functionality Update

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,10 +1,21 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
 
+const baseQuery = fetchBaseQuery({
+  baseUrl: "https://islamic-university-api.vercel.app/api/v1",
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    // Get Token From Redux State
+    const token = (getState() as RootState).auth.token;
+    // Set Header Token
+    if (token) {
+      headers.set("authorization", `${token}`);
+    }
+    return headers;
+  },
+});
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "https://islamic-university-api.vercel.app/api/v1",
-    credentials: "include",
-  }),
+  baseQuery: baseQuery,
   endpoints: () => ({}),
 });
